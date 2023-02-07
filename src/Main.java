ddac1c59d1bde59ac4c9c576b6fef087fba17bc4
Drogolova <drogolova1997@mail.ru>
2023-02-07 23:25:44 +0300

    функциональное программирование выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        List<String> words = Arrays.asList(string.split(" "));
+        System.out.println("В тексте " + words.size() + " слов");
+        System.out.println("ТОП 10:");
+        words.stream().collect(Collectors.groupingBy(word -> word, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Comparator.comparing(Map.Entry::getKey))
+                .sorted(Comparator.comparingLong(Map.Entry<String, Long>::getValue).reversed())
+                .limit(10)
+                .forEachOrdered(pair -> System.out.println(pair.getValue() + "-" + pair.getKey()));
     }
 }
